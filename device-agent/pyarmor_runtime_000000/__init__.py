998613911d70f84f144eaef358b29431af029d2f
Caroline Ribeiro <caroline.ribeiro@sitiobarreiras.com>
2025-01-11 21:41:22 -0300

    feat: pushing folder and tar.gz

---
+++ device-agent/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.0.7 (trial), 000000, 2025-01-11T21:40:52.731286
+from .pyarmor_runtime import __pyarmor__
--- pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 9.0.7 (trial), 000000, 2025-01-11T21:19:23.080906
-from .pyarmor_runtime import __pyarmor__
